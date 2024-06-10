e32ca0adbe9475114e715fafaf043c7e1fb96671
Maarten Behn <maarten.behn@gmail.com>
2024-06-10 21:01:52 +0200

    stuff

--- projects/space_ship_builder_v7/src/rules/block.rs
+++ projects/space_ship_builder_v7/src/rules/block.rs
@@ -1,5 +1,11 @@
+use crate::math::{oct_positions, to_1d, to_1d_i};
 use crate::node::NodeID;
+use crate::rotation::Rot;
+use crate::rules::Rules;
 use octa_force::anyhow::bail;
+use octa_force::glam::{BVec3, IVec3, Mat4};
+use std::mem;
+use std::ops::Mul;
 
 pub type BlockNameIndex = usize;
 pub const BLOCK_INDEX_EMPTY: BlockNameIndex = 0;
@@ -23,5 +29,52 @@ impl Block {
         }
 
         Self { node_ids }
+    }
+
+    pub fn flip(&self, flip: BVec3, rules: &mut Rules) -> Block {
+        let mut node_ids = self.node_ids.to_owned();
+
+        if flip.x {
+            node_ids.swap(0, 1);
+            node_ids.swap(2, 3);
+            node_ids.swap(4, 5);
+            node_ids.swap(6, 7);
+        }
+
+        if flip.y {
+            node_ids.swap(0, 2);
+            node_ids.swap(1, 3);
+            node_ids.swap(4, 6);
+            node_ids.swap(5, 7);
+        }
+
+        if flip.z {
+            node_ids.swap(0, 4);
+            node_ids.swap(1, 5);
+            node_ids.swap(2, 6);
+            node_ids.swap(3, 7);
+        }
+
+        Block::from_node_ids(node_ids)
+    }
+
+    pub fn rotate(&self, rot: Rot, rules: &mut Rules) -> Block {
+        let mat: Mat4 = rot.into();
+
+        let mut rotated_node_ids = [NodeID::empty(); 8];
+        for (node_id, pos) in self.node_ids.iter().zip(oct_positions().iter()) {
+            let rotated_node_id = NodeID::new(node_id.index, node_id.rot.mul(rot));
+            let rotated_node_id = rules.get_duplicate_node_id(rotated_node_id);
+
+            let rotated_pos = mat
+                .transform_vector3((*pos - IVec3::ONE).as_vec3())
+                .round()
+                .as_ivec3()
+                + IVec3::ONE;
+            let index = to_1d_i(rotated_pos, IVec3::ONE * 2) as usize;
+            rotated_node_ids[index] = rotated_node_id;
+        }
+
+        Block::from_node_ids(rotated_node_ids)
     }
 }
--- projects/space_ship_builder_v7/src/rules/hull.rs
+++ projects/space_ship_builder_v7/src/rules/hull.rs
@@ -92,144 +92,39 @@ impl HullSolver {
             ],
         ];
 
+        let mut base_blocks = vec![];
         for (i, req) in hull_reqs.into_iter().enumerate() {
             let block = rules
                 .load_block_from_node_folder(&format!("{HULL_BASE_NAME_PART}-{i}"), voxel_loader)?;
 
-            self.base_blocks.push((req, block));
+            base_blocks.push((req, block));
         }
+
+        let mut rotated_base_blocks = permutate_base_blocks(&base_blocks, rules);
+        self.base_blocks.append(&mut rotated_base_blocks);
 
         Ok(())
     }
 }
 
-fn flip_block_req(
-    node_id: &NodeID,
-    block_req: &[(IVec3, BlockNameIndex)],
-    flips: &[BVec3],
-) -> Vec<(NodeID, Vec<(IVec3, BlockNameIndex)>)> {
-    let mut rotated_rules = Vec::new();
-
-    for flip in flips.iter() {
-        let flip_a = ivec3(
-            if flip.x { -1 } else { 1 },
-            if flip.y { -1 } else { 1 },
-            if flip.z { -1 } else { 1 },
-        );
-        let flip_b = ivec3(
-            if flip.x { 1 } else { 0 },
-            if flip.y { 1 } else { 0 },
-            if flip.z { 1 } else { 0 },
-        );
-        let flipped_rot = node_id.rot.flip(flip.to_owned());
-
-        let flippped_req: Vec<_> = block_req
-            .iter()
-            .map(|(pos, indecies)| {
-                let flipped_pos = ((*pos) + flip_b) * flip_a;
-                (flipped_pos, indecies.to_owned())
-            })
-            .collect();
-
-        rotated_rules.push((NodeID::new(node_id.index, flipped_rot), flippped_req))
-    }
-
-    rotated_rules
-}
-
-fn rotate_block_req(
-    node_id: &NodeID,
-    block_req: &[(IVec3, BlockNameIndex)],
-    rotates: &[BVec3],
-) -> Vec<(NodeID, Vec<(IVec3, BlockNameIndex)>)> {
-    let mut rotated_rules = Vec::new();
-
-    for &rotate in rotates {
-        let mat = Mat4::from_mat3(node_id.rot.into());
-
-        let rot_mat_x = if rotate.x {
-            Mat4::from_mat3(Mat3::from_cols_array(&[
-                1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
-            ]))
-        } else {
-            Mat4::IDENTITY
-        };
-        let rot_mat_y = if rotate.y {
-            Mat4::from_mat3(Mat3::from_cols_array(&[
-                0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0,
-            ]))
-        } else {
-            Mat4::IDENTITY
-        };
-        let rot_mat_z = if rotate.z {
-            Mat4::from_mat3(Mat3::from_cols_array(&[
-                0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0,
-            ]))
-        } else {
-            Mat4::IDENTITY
-        };
-
-        // Yeah, glsl rotation order is different to glam, so I just create two different matrices.
-        // I know ... but it's the simplest fix.
-        let pos_mat = rot_mat_x * rot_mat_y * rot_mat_z;
-        let rot_mat = rot_mat_z * rot_mat_y * rot_mat_x;
-
-        let rotated_rot: Rot = Mat3::from_mat4(mat * rot_mat).into();
-
-        let rotated_req: Vec<_> = block_req
-            .iter()
-            .map(|(pos, indecies)| {
-                let rotated_pos = pos_mat.transform_point3(pos.as_vec3()).round().as_ivec3();
-                (rotated_pos, indecies.to_owned())
-            })
-            .collect();
-
-        rotated_rules.push((NodeID::new(node_id.index, rotated_rot), rotated_req));
-    }
-
-    rotated_rules
-}
-
-fn permutate_block_req(
-    block_reqs: &[(NodeID, Prio, Vec<(IVec3, BlockNameIndex)>)],
+fn permutate_base_blocks(
+    blocks: &[(Vec<IVec3>, Block)],
     rules: &mut Rules,
-) -> Vec<(NodeID, Prio, Vec<(IVec3, BlockNameIndex)>)> {
-    let rotations = all_bvec3s();
-    let flips = all_bvec3s();
+) -> Vec<(Vec<IVec3>, Block)> {
+    let mut rotated_blocks = vec![];
+    for (reqs, block) in blocks.iter() {
+        for rot in Rot::IDENTITY.get_all_permutations() {
+            let mat: Mat4 = rot.into();
+            let rotated_reqs: Vec<_> = reqs
+                .iter()
+                .map(|req| mat.transform_vector3((*req).as_vec3()).round().as_ivec3())
+                .collect();
 
-    let mut permutated_block_reqs: Vec<(NodeID, Prio, Vec<(IVec3, BlockNameIndex)>)> = Vec::new();
+            let rotated_block = block.rotate(rot, rules);
 
-    for (node_id, prio, block_reqs) in block_reqs.iter() {
-        let flipped_rules = flip_block_req(node_id, block_reqs, &flips);
-
-        for (flipped_node_id, flipped_req) in flipped_rules {
-            let rotated_rules = rotate_block_req(&flipped_node_id, &flipped_req, &rotations);
-
-            for (permutated_node_id, permutated_req) in rotated_rules {
-                let permutated_node_id = rules.get_duplicate_node_id(permutated_node_id);
-
-                let mut added = false;
-                for (test_id, _, test_reqs) in permutated_block_reqs.iter() {
-                    if permutated_node_id != *test_id {
-                        continue;
-                    }
-
-                    if *test_reqs == permutated_req {
-                        added = true;
-                        break;
-                    }
-                }
-
-                if !added {
-                    permutated_block_reqs.push((
-                        permutated_node_id,
-                        prio.to_owned(),
-                        permutated_req,
-                    ));
-                }
-            }
+            rotated_blocks.push((rotated_reqs, rotated_block))
         }
     }
 
-    permutated_block_reqs
+    rotated_blocks
 }
